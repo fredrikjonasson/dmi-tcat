9969df7883e6256681063e30540aa1ef5a32b105
Fredrik <fredrik.jonasson@outlook.com>
2019-05-22 14:59:45 +0200

    Tidying the code

--- analysis/common/pseudonymization.php
+++ analysis/common/pseudonymization.php
@@ -59,319 +59,44 @@ function is_pseudonymized($dataset) {
 	return $boolindicator;
 }
 
+function pseudonymize_field($pseudo_list, $data, $datakey, $last_pseudo_index){
+    if (array_key_exists($datakey, $data) && ($data[$datakey] != NULL)) {
+        $mask = array_search($data[$datakey],array_column($pseudo_list, 'original_data' ));
+        if ($mask) {
+            $data[$datakey] = $mask;
+        } else {
+            $newData = array(
+                'original_data' => $data[$datakey],
+                'fieldtype' => $datakey
+            );
+            $pseudo_list[($last_pseudo_index+1)] = $newData;
+            $data[$datakey]=($last_pseudo_index+1);
+            $last_pseudo_index = ($last_pseudo_index + 1);
+        }
+    }
+    $argument_array = array(
+        'last_pseudo_index' => $last_pseudo_index,
+        'data' => $data,
+        'pseudo_list' => $pseudo_list 
+    );
+    return $argument_array;
+}
+
 function pseudonymize($data) {  
     $pseudo_list = fetch_pseudonymized_data();
     end($pseudo_list);
     $insert_start_value = $last_pseudo_index = key($pseudo_list);
+    // If the key to the $data is in the $key_array below, then it will be handled by the foreach.
+    $key_array=array('id', 'tweetid', 'id_string', 'from_user_id', 'from_user_name', 'from_user_realname', 'user_from_name', 'user_from_id', 'user_to_id', 'user_to_name', 'to_user_id', 'to_user_name', 'in_reply_to_status_id', 'in_reply_to_status_id_str', 'in_reply_to_user_id', 'in_reply_to_screen_name', 'quoted_status_id', 'retweeted_status', 'retweeted', 'retweet_id' );
+    foreach ($data as $key => $value) {
+        if (array_key_exists($key, $data) && ($value != NULL) && in_array($key, $key_array)) {
+            $argument_array =pseudonymize_field($pseudo_list, $data, $key, $last_pseudo_index);
+            $last_pseudo_index = $argument_array['last_pseudo_index'];
+            $data = $argument_array['data'];
+            $pseudo_list = $argument_array['pseudo_list']; 
+            }        
+    }
 
-    if (array_key_exists('tweetid', $data) && ($data['tweetid'] != NULL)) {
-        $mask = array_search($data['tweetid'],array_column($pseudo_list, 'original_data' ));
-        if ($mask) {
-            $data['tweetid'] = $mask;
-        } else {
-            $newData = array(
-                'original_data' => $data['tweetid'],
-                'fieldtype' => 'tweetid'
-            );
-            $pseudo_list[($last_pseudo_index+1)] = $newData;
-            $data[('tweetid')]=($last_pseudo_index+1);
-            $last_pseudo_index = ($last_pseudo_index + 1);
-        }
-    }   
-    if (array_key_exists('id', $data) && ($data['id'] != NULL)) {
-        $mask = array_search($data['id'],array_column($pseudo_list, 'original_data' ));
-        if ($mask) {
-            $data['id'] = $mask;
-        } else {
-            $newData = array(
-                'original_data' => $data['id'],
-                'fieldtype' => 'id'
-            );
-            $pseudo_list[($last_pseudo_index+1)] = $newData;
-            $data[('id')]=($last_pseudo_index+1);
-            $last_pseudo_index = ($last_pseudo_index + 1);
-        }
-    }
-    if (array_key_exists('id_string', $data) && ($data['id_string'] != NULL)) {
-        $mask = array_search($data['id_string'],array_column($pseudo_list, 'original_data' ));
-        if ($mask) {
-            $data['id_string'] = $mask;
-        } else {
-            $newData = array(
-                'original_data' => $data['id_string'],
-                'fieldtype' => 'id_string'
-            );
-            $pseudo_list[($last_pseudo_index+1)] = $newData;
-            $data[('id_string')]=($last_pseudo_index+1);
-            $last_pseudo_index = ($last_pseudo_index + 1);
-        }
-    }
-    if (array_key_exists('from_user_id', $data) && ($data['from_user_id'] != NULL)) {
-        $mask = array_search($data['from_user_id'],array_column($pseudo_list, 'original_data' ));
-        if ($mask) {
-            $data['from_user_id'] = $mask;
-        } else {
-            $newData = array(
-                'original_data' => $data['from_user_id'],
-                'fieldtype' => 'from_user_id'
-            );
-            $pseudo_list[($last_pseudo_index+1)] = $newData;
-            $data[('from_user_id')]=($last_pseudo_index+1);
-            $last_pseudo_index = ($last_pseudo_index + 1);
-        }
-    }
-    if (array_key_exists('from_user_name', $data) && ($data['from_user_name'] != NULL)) {
-        $mask = array_search($data['from_user_name'],array_column($pseudo_list, 'original_data' ));
-        if ($mask) {
-            $data['from_user_name'] = $mask;
-        } else {
-            $newData = array(
-                'original_data' => $data['from_user_name'],
-                'fieldtype' => 'from_user_name'
-            );
-            $pseudo_list[($last_pseudo_index+1)] = $newData;
-            $data[('from_user_name')]=($last_pseudo_index+1);
-            $last_pseudo_index = ($last_pseudo_index + 1);
-        }
-    }
-    if (array_key_exists('from_user_realname', $data) && ($data['from_user_realname'] != NULL)) {
-        $mask = array_search($data['from_user_realname'],array_column($pseudo_list, 'original_data' ));
-        if ($mask) {
-            $data['from_user_realname'] = $mask;
-        } else {
-            $newData = array(
-                'original_data' => $data['from_user_realname'],
-                'fieldtype' => 'from_user_realname'
-            );
-            $pseudo_list[($last_pseudo_index+1)] = $newData;
-            $data[('from_user_realname')]=($last_pseudo_index+1);
-            $last_pseudo_index = ($last_pseudo_index + 1);
-        }
-    }
-    if (array_key_exists('user_from_name', $data) && ($data['user_from_name'] != NULL)) {
-        $mask = array_search($data['user_from_name'],array_column($pseudo_list, 'original_data' ));
-        if ($mask) {
-            $data['user_from_name'] = $mask;
-        } else {
-            $newData = array(
-                'original_data' => $data['user_from_name'],
-                'fieldtype' => 'user_from_name'
-            );
-            $pseudo_list[($last_pseudo_index+1)] = $newData;
-            $data[('user_from_name')]=($last_pseudo_index+1);
-            $last_pseudo_index = ($last_pseudo_index + 1);
-        }
-    }
-    if (array_key_exists('user_from_id', $data) && ($data['user_from_id'] != NULL)) {
-        $mask = array_search($data['user_from_id'],array_column($pseudo_list, 'original_data' ));
-        if ($mask) {
-            $data['user_from_id'] = $mask;
-        } else {
-            $newData = array(
-                'original_data' => $data['user_from_id'],
-                'fieldtype' => 'user_from_id'
-            );
-            $pseudo_list[($last_pseudo_index+1)] = $newData;
-            $data[('user_from_id')]=($last_pseudo_index+1);
-            $last_pseudo_index = ($last_pseudo_index + 1);
-        }
-    }
-    if (array_key_exists('user_to_id', $data) && ($data['user_to_id'] != NULL)) {
-        $mask = array_search($data['user_to_id'],array_column($pseudo_list, 'original_data' ));
-        if ($mask) {
-            $data['user_to_id'] = $mask;
-        } else {
-            $newData = array(
-                'original_data' => $data['user_to_id'],
-                'fieldtype' => 'user_to_id'
-            );
-            $pseudo_list[($last_pseudo_index+1)] = $newData;
-            $data[('user_to_id')]=($last_pseudo_index+1);
-            $last_pseudo_index = ($last_pseudo_index + 1);
-        }
-    }
-    if (array_key_exists('user_to_name', $data) && ($data['user_to_name'] != NULL)) {
-        $mask = array_search($data['user_to_name'],array_column($pseudo_list, 'original_data' ));
-        if ($mask) {
-            $data['user_to_name'] = $mask;
-        } else {
-            $newData = array(
-                'original_data' => $data['user_to_name'],
-                'fieldtype' => 'user_to_name'
-            );
-            $pseudo_list[($last_pseudo_index+1)] = $newData;
-            $data[('user_to_name')]=($last_pseudo_index+1);
-            $last_pseudo_index = ($last_pseudo_index + 1);
-        }
-    }
-    if (array_key_exists('to_user_id', $data) && ($data['to_user_id'] != NULL)) {
-        $mask = array_search($data['to_user_id'],array_column($pseudo_list, 'original_data' ));
-        if ($mask) {
-            $data['to_user_id'] = $mask;
-        } else {
-            $newData = array(
-                'original_data' => $data['to_user_id'],
-                'fieldtype' => 'to_user_id'
-            );
-            $pseudo_list[($last_pseudo_index+1)] = $newData;
-            $data[('to_user_id')]=($last_pseudo_index+1);
-            $last_pseudo_index = ($last_pseudo_index + 1);
-        }
-    }
-    if (array_key_exists('to_user_name', $data) && ($data['to_user_name'] != NULL)) {
-        $mask = array_search($data['to_user_name'],array_column($pseudo_list, 'original_data' ));
-        if ($mask) {
-            $data['to_user_name'] = $mask;
-        } else {
-            $newData = array(
-                'original_data' => $data['to_user_name'],
-                'fieldtype' => 'to_user_name'
-            );
-            $pseudo_list[($last_pseudo_index+1)] = $newData;
-            $data[('to_user_name')]=($last_pseudo_index+1);
-            $last_pseudo_index = ($last_pseudo_index + 1);
-        }
-    }
-    if (array_key_exists('in_reply_to_status_id', $data) && ($data['in_reply_to_status_id'] != NULL)) {
-        $mask = array_search($data['in_reply_to_status_id'],array_column($pseudo_list, 'original_data' ));
-        if ($mask) {
-            $data['in_reply_to_status_id'] = $mask;
-        } else {
-            $newData = array(
-                'original_data' => $data['in_reply_to_status_id'],
-                'fieldtype' => 'in_reply_to_status_id'
-            );
-            $pseudo_list[($last_pseudo_index+1)] = $newData;
-            $data[('in_reply_to_status_id')]=($last_pseudo_index+1);
-            $last_pseudo_index = ($last_pseudo_index + 1);
-        }
-    }
-    if (array_key_exists('in_reply_to_status_id_str', $data) && ($data['in_reply_to_status_id_str'] != NULL)) {
-        $mask = array_search($data['in_reply_to_status_id_str'],array_column($pseudo_list, 'original_data' ));
-        if ($mask) {
-            $data['in_reply_to_status_id_str'] = $mask;
-        } else {
-            $newData = array(
-                'original_data' => $data['in_reply_to_status_id_str'],
-                'fieldtype' => 'in_reply_to_status_id_str'
-            );
-            $pseudo_list[($last_pseudo_index+1)] = $newData;
-            $data[('in_reply_to_status_id_str')]=($last_pseudo_index+1);
-            $last_pseudo_index = ($last_pseudo_index + 1);
-        }
-    }
-    if (array_key_exists('in_reply_to_user_id', $data) && ($data['in_reply_to_user_id'] != NULL)) {
-        $mask = array_search($data['in_reply_to_user_id'],array_column($pseudo_list, 'original_data' ));
-        if ($mask) {
-            $data['in_reply_to_user_id'] = $mask;
-        } else {
-            $newData = array(
-                'original_data' => $data['in_reply_to_user_id'],
-                'fieldtype' => 'in_reply_to_user_id'
-            );
-            $pseudo_list[($last_pseudo_index+1)] = $newData;
-            $data[('in_reply_to_user_id')]=($last_pseudo_index+1);
-            $last_pseudo_index = ($last_pseudo_index + 1);
-        }
-    }
-    if (array_key_exists('in_reply_to_screen_name', $data) && ($data['in_reply_to_screen_name'] != NULL)) {
-        $mask = array_search($data['in_reply_to_screen_name'],array_column($pseudo_list, 'original_data' ));
-        if ($mask) {
-            $data['in_reply_to_screen_name'] = $mask;
-        } else {
-            $newData = array(
-                'original_data' => $data['in_reply_to_screen_name'],
-                'fieldtype' => 'in_reply_to_screen_name'
-            );
-            $pseudo_list[($last_pseudo_index+1)] = $newData;
-            $data[('in_reply_to_screen_name')]=($last_pseudo_index+1);
-            $last_pseudo_index = ($last_pseudo_index + 1);
-        }
-    }
-    if (array_key_exists('to_user_name', $data) && ($data['to_user_name'] != NULL)) {
-        $mask = array_search($data['to_user_name'],array_column($pseudo_list, 'original_data' ));
-        if ($mask) {
-            $data['to_user_name'] = $mask;
-        } else {
-            $newData = array(
-                'original_data' => $data['to_user_name'],
-                'fieldtype' => 'to_user_name'
-            );
-            $pseudo_list[($last_pseudo_index+1)] = $newData;
-            $data[('to_user_name')]=($last_pseudo_index+1);
-            $last_pseudo_index = ($last_pseudo_index + 1);
-        }
-    }
-    if (array_key_exists('in_reply_to_status_id', $data) && ($data['in_reply_to_status_id'] != NULL)) {
-        $mask = array_search($data['in_reply_to_status_id'],array_column($pseudo_list, 'original_data' ));
-        if ($mask) {
-            $data['in_reply_to_status_id'] = $mask;
-        } else {
-            $newData = array(
-                'original_data' => $data['in_reply_to_status_id'],
-                'fieldtype' => 'in_reply_to_status_id'
-            );
-            $pseudo_list[($last_pseudo_index+1)] = $newData;
-            $data[('in_reply_to_status_id')]=($last_pseudo_index+1);
-            $last_pseudo_index = ($last_pseudo_index + 1);
-        }
-    }
-    if (array_key_exists('quoted_status_id', $data) && ($data['quoted_status_id'] != NULL)) {
-        $mask = array_search($data['quoted_status_id'],array_column($pseudo_list, 'original_data' ));
-        if ($mask) {
-            $data['quoted_status_id'] = $mask;
-        } else {
-            $newData = array(
-                'original_data' => $data['quoted_status_id'],
-                'fieldtype' => 'quoted_status_id'
-            );
-            $pseudo_list[($last_pseudo_index+1)] = $newData;
-            $data[('quoted_status_id')]=($last_pseudo_index+1);
-            $last_pseudo_index = ($last_pseudo_index + 1);
-        }
-    }
-    if (array_key_exists('retweeted_status', $data) && ($data['retweeted_status'] != NULL)) {
-        $mask = array_search($data['retweeted_status'],array_column($pseudo_list, 'original_data' ));
-        if ($mask) {
-            $data['retweeted_status'] = $mask;
-        } else {
-            $newData = array(
-                'original_data' => $data['retweeted_status'],
-                'fieldtype' => 'retweeted_status'
-            );
-            $pseudo_list[($last_pseudo_index+1)] = $newData;
-            $data[('retweeted_status')]=($last_pseudo_index+1);
-            $last_pseudo_index = ($last_pseudo_index + 1);
-        }
-    }
-    if (array_key_exists('retweeted', $data) && ($data['retweeted'] != NULL)) {
-        $mask = array_search($data['retweeted'],array_column($pseudo_list, 'original_data' ));
-        if ($mask) {
-            $data['retweeted'] = $mask;
-        } else {
-            $newData = array(
-                'original_data' => $data['retweeted'],
-                'fieldtype' => 'retweeted'
-            );
-            $pseudo_list[($last_pseudo_index+1)] = $newData;
-            $data[('retweeted')]=($last_pseudo_index+1);
-            $last_pseudo_index = ($last_pseudo_index + 1);
-        }
-    }
-    if (array_key_exists('retweet_id', $data) && ($data['retweet_id'] != NULL)) {
-        $mask = array_search($data['retweet_id'], array_column($pseudo_list, 'original_data' ));
-        if ($mask) {
-            $data['retweet_id'] = $mask;
-        } else {
-            $newData = array(
-                'original_data' => $data['retweet_id'],
-                'fieldtype' => 'retweet_id'
-            );
-            $pseudo_list[($last_pseudo_index+1)] = $newData;
-            $data[('retweet_id')]=($last_pseudo_index+1);
-            $last_pseudo_index = ($last_pseudo_index + 1);
-        }
-    }
     if (array_key_exists('from_user_profile_image_url', $data) && ($data['from_user_profile_image_url'] != NULL)) {
         $data['from_user_profile_image_url'] = "Omitted, see original table";
     }
@@ -397,8 +122,7 @@ function pseudonymize($data) {
             }
     }
     save_pseudonymized_data($pseudo_list, $insert_start_value , $last_pseudo_index);
-    return $data;
-    
+    return $data;   
 }
 	
 	
